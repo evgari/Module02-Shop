d489878eaffeb1e6aacf42d2e0cc3c2f12990ab7
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-10-30 16:32:51 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import 'normalize.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 3,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
+
