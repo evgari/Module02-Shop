36d70beaa17ac2fef3db2e1fb3023727d19f98de
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-11-13 10:15:47 +0300

    adaptive ready

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import './styles/main.scss';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper(".offerSwiper", {
-  slidesPerView: 3,
+const swiper = new Swiper(".js-offer-swiper", {
   spaceBetween: 20,
-  slidesPerGroup: 3,
   loop: false,
   pagination: {
     el: ".swiper-pagination",
@@ -21,12 +19,24 @@ const swiper = new Swiper(".offerSwiper", {
     prevEl: ".swiper-button-prev",
   },
   modules: [Navigation, Pagination],
+  breakpoints: {
+    321: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1001: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    }
+  }
 });
 
-const swiper2 = new Swiper(".trendsSwiper", {
-  slidesPerView: 3,
+const swiper2 = new Swiper(".js-trends-swiper", {
   spaceBetween: 20,
-  slidesPerGroup: 3,
   loop: false,
   pagination: {
     el: ".swiper-pagination",
@@ -37,6 +47,57 @@ const swiper2 = new Swiper(".trendsSwiper", {
     prevEl: ".swiper-button-prev",
   },
   modules: [Navigation, Pagination],
+  breakpoints: {
+    321: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1001: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    }
+  }
+});
+
+const swiper3 = document.querySelector('.js-category-list');
+
+let mySwiper;
+
+function mobileSlider() {
+  if (window.innerWidth <= 768 && swiper3.dataset.mobile == 'false') {
+    mySwiper = new Swiper(swiper3, {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      breakpoints: {
+        501: {
+          slidesPerView: 2.5,
+          spaceBetween: 10,
+        },
+        601: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        }
+      },
+    });
+
+    swiper3.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth > 768) {
+    swiper3.dataset.mobile = 'false';
+
+    mySwiper.destroy();
+  }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+  mobileSlider();
 });
 
 document.querySelector('.catalog__btn').addEventListener('click', e => {
